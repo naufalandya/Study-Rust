1324b3154971f076f58a2252e35e08918cc5b2fe
naufalandya <135545118+naufalandya@users.noreply.github.com>
2024-10-12 21:12:37 +0700

    array & tupple, data type scalar & compound

--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,101 @@
-// use std::io;
 use std::io::{self, Write};
-use rand::Rng;
-use std::cmp::Ordering;
 
 fn main(){
-    let name: &str = "Naufal Andya Faiz";
+    let mut name = String::new();
 
-    println!("Hello world {}", name);
-
-    print!("What is your age ? ");
+    print!("Please enter your name : ");
     io::stdout()
         .flush()
-        .expect("Failed to flush");
-
-    let mut age: String = String::new();
-
-    // print!("What is your age"); //jangan disni
+        .expect("failed to flush");
 
     io::stdin()
-        .read_line(&mut age)
-        .expect("failed to read line");
+        .read_line(&mut name)
+        .expect("Failed name");
 
-    println!("Your name is {} with age {}", name, age);
- 
-    let x = 5;
-    let age: i32 = age.trim().parse().expect("please enter number!");
+    println!("Your name is {name}");
 
-    let y: i32;
-    y = x + age;
-    println!("If the value of x is {x}, and your age is {age}, then total of y (lucky number) = x + age + 10 (random anomaly number lol) is {}", y + 10);
+    //kalo const tipe data nya harus dijelasin
+    const X: i32 = 5;
+    const Y: i32 = 10;
+    const Z: i32 = X + Y;
 
+    println!("hasil x + y adalah {Z}");
 
-    // Shadowing
-    let age: String = "20".to_string();
-    println!("String age: {}", age); // age is a String
+    // Tipe data kalau bisa langsung di deklrasikan
 
-    let age: i32 = age.trim().parse().unwrap();
-    println!("Integer age: {}", age); // age is now an i32 after shadowing
+    let angka_satu: i32 = 1500;
+    let angka_dua: i32 = 1090;
+    let total = angka_dua + angka_dua + angka_satu;
+    println!("{total}");
 
-    println!("But hey now lets play guessing games !");
+    let angka_satu: u32 = "42".parse().expect("Not a number!");
+    println!("{angka_satu}");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    //skalar
 
-    println!("the secret number is {secret_number}");
-
-    loop { //allowing multiple input
-
-    print!("please input your number : ");
-    io::stdout()
-        .flush()
-        .expect("Failed to flush");
+    // A scalar type represents a single value. 
+    // Rust has four primary scalar types: integers, floating-point numbers, Booleans, and characters. 
+    // You may recognize these from other programming languages. Let’s jump into how they work in Rust.
 
 
-    let mut number = String::new();
+    //number
 
-    io::stdin()
-        .read_line(&mut number)
-        .expect("failed to read line");
+    // addition
+    let sum = 5 + 10;
 
-    println!("you guessed {number}");
+    // subtraction
+    let difference = 95.5 - 4.3;
 
-    //shadowing again
+    // multiplication
+    let product = 4 * 30;
 
-        let number: u32 = number.trim().parse().expect("Please type a number!");
+    // division
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3; // Results in -1
 
-        //Comparing
-    
-        match number.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");     
-                break; //stop loop
-    
-            }
-        }
-    }
+    // remainder
+    let remainder = 43 % 5;
+
+    println!("{sum}, {difference}, {product}, {quotient}, {truncated}, {remainder}");
+
+    // Boolean
+
+    let t = true;
+
+    let f: bool = false; // with explicit type annotation
+
+    println!("{t}, {f}");
+
+    // Char
+    let c = "z"; //bukan char
+    let d = 'A';
+    let z: char = 'ℤ'; // with explicit type annotation
+    let heart_eyed_cat = '😻';
+
+    println!("{} {} {} {}", c, d, z, heart_eyed_cat);
+    println!("{}", c);
+
+
+    // Compound Types
+    // Compound types can group multiple values into one type. Rust has two primitive compound types: tuples and arrays.
+
+    //Tuple
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    println!(" {x} {y} {z} ");
+
+    let satu = tup.0;
+    let dua = tup.1;
+    let tiga = tup.2;
+
+    println!(" {satu} {dua} {tiga} ");
+
+    //Array
+
+    let days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+    println!("{:?}", days); // Print the array of days
+
+    let nums: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("{:?}", nums); // Print the array of numbers
 
 }
